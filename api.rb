9ed1522e67755d486a5345b54cffdf6efe689d84
smoc-buk <soliva@buk.cl>
2024-10-25 13:48:34 -0300

    add rescue api

--- api.rb
+++ api.rb
@@ -10,27 +10,31 @@ require_relative 'cosine_similarity'
 DBHandler.setup_database
 
 get '/api/v1/consulta/:query' do
-  # params
-  content_type :json
-  query = params[:query]
+  begin
+    # params
+    content_type :json
+    query = params[:query]
 
-  # params query
-  consulta_embedding = EmbeddingGenerator.get_embedding(query)
-  
-  # result
-  tickets_similares = DBHandler.find_similar_tickets(consulta_embedding)
+    # params query
+    consulta_embedding = EmbeddingGenerator.get_embedding(query)
+    
+    # result
+    tickets_similares = DBHandler.find_similar_tickets(consulta_embedding)
 
-  tickets_json = []
-  tickets_similares.first(3).each do |ticket|
-    solution = ticket[:analisis] if ticket[:analisis] rescue ''
-    ticket_json = {
-      ticket_id: ticket[:ticket_id],
-      score: ticket[:similitud].round(2),
-      title: ticket[:titulo],
-      description: ticket[:descripcion],
-      solution: solution
-    }
-    tickets_json << ticket_json
+    tickets_json = []
+    tickets_similares.first(3).each do |ticket|
+      solution = ticket[:analisis] if ticket[:analisis] rescue ''
+      ticket_json = {
+        ticket_id: ticket[:ticket_id],
+        score: ticket[:similitud].round(2),
+        title: ticket[:titulo],
+        description: ticket[:descripcion],
+        solution: solution
+      }
+      tickets_json << ticket_json
+    end
+    { tickets: tickets_json }.to_json
+  rescue StandardError => e
+    {error: "Lo siento, no tenemos esa informacion :("}
   end
-  { tickets: tickets_json }.to_json
 end
